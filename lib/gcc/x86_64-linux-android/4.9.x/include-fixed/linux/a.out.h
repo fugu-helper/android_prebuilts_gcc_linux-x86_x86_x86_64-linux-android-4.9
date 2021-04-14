07f15897d361291af52e6672796d4fdfdca50294
deadman96385 <seanhoyt963@gmail.com>
2021-04-14 04:06:45 -0500

    Add apollo-lake changes

--- lib/gcc/x86_64-linux-android/4.9.x/include-fixed/linux/a.out.h
+++ lib/gcc/x86_64-linux-android/4.9.x/include-fixed/linux/a.out.h
@@ -2,7 +2,7 @@
 
     It has been auto-edited by fixincludes from:
 
-	"/tmp/05b9da874e1df89efdf91015abc584b2/sysroot/usr/include/linux/a.out.h"
+	"/tmp/a90fe1b6c9e368f5befa927c81a4b7f4/sysroot/usr/include/linux/a.out.h"
 
     This had to be done to correct non-standard usages in the
     original, manufacturer supplied header file.  */
